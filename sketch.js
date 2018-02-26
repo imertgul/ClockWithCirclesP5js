1087b38582d8d57cacd05f45fa87c9da63123885
Mert Gul <mert456274@gmail.com>
2018-02-26 20:47:06 +0300

    Done!
    Now it is responsive and also clock is working well eventually :)

--- sketch.js
+++ sketch.js
@@ -1,30 +1,27 @@
 var canvas;
-
+var thickness=20;
 function setup(){
     canvas = createCanvas(500,500);
 }
 
 function draw(){
     background(2500);
-    var h=hour();
+    var h=hour()%12;
     var m=minute();
     var s=second();
-    h=h%12;
-    strokeWeight(8);
+
+    strokeWeight(thickness);
     stroke(199,103,99);
     noFill();
-    var ha=map(h,0,12,0,360);
-    arc(250, 250, 380, 380, 0-HALF_PI, ha-HALF_PI);
-
-
+    var ha=map(h,0,12,0,TWO_PI);
+    arc(width/2, height/2, width/2+thickness*2-thickness*2, width/2+thickness*2-thickness*2,  0-HALF_PI, ha-HALF_PI);
 
     stroke(103,153,150);
-    var ma=map(m,0,60,0,360);
-    arc(250, 250, 400, 400, 0+3*HALF_PI, m);
-    console.log(m,ma);
+    var ma=map(m,0,60,0,TWO_PI);
+    arc(width/2, height/2, width/2+thickness*2, width/2+thickness*2, 0-HALF_PI, ma-HALF_PI);
 
     stroke(153,103,199);
-    var sa=map(s,0,60,0,360);
-    arc(250, 250, 420, 420, 0-HALF_PI, sa-HALF_PI);
+    var sa=map(s,0,60,0,TWO_PI);
+    arc(width/2, height/2, width/2+thickness*2+thickness*2, width/2+thickness*2+thickness*2, 0-HALF_PI, sa-HALF_PI);
 
 }
